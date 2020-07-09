4e7988c9aef4f24a80454992bec4e5c16f9689c7
braresa <lucasgabrielcontatoc@gmail.com>
2020-07-09 00:16:12 -0300

    Test.

--- main.js
+++ main.js
@@ -1,3 +1,22 @@
+
+const debounce = function(func, wait, immediate) {
+    let timeout;
+    return function(...args) {
+        const context = this;
+        const later = function () {
+            timeout = null;
+            if (!immediate) func.apply(context, args);
+        }
+        const callNow = immediate && !timeout;
+        clearTimeout(timeout)
+        timeout = setTimeout(later, wait);
+        if (callNow) func.apply(context, args);
+    }
+}
+
+
+
+
 const targetAnim = document.querySelectorAll('[data-aboutanim]');
 const classAnimation = 'animate';
 
@@ -6,6 +25,7 @@ function aboutMeScroll() {
     targetAnim.forEach(function(element) {
             if((windowPosition) > element.offsetTop) {
                 element.classList.add(classAnimation);
+                console.log("animation rolled")
             }
             else {
                 element.classList.remove(classAnimation);
@@ -14,6 +34,11 @@ function aboutMeScroll() {
 
 }
 
-window.addEventListener('scroll', function() {
+aboutMeScroll();
+
+
+if(targetAnim.length) {
+window.addEventListener('scroll', debounce(function() {
     aboutMeScroll();
-})
+}, 30));
+}

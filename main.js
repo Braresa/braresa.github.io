02ecde5a68f8e443c605c117c033c4c64c3d3f62
braresa <lucasgabrielcontatoc@gmail.com>
2020-07-08 23:24:15 -0300

    0.40 otimização no código, adicionado animações pra futuros textos.
    Entendi o conceito do HTML e agora tô onfire, ninguem me segura papai.....

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
-// nothing (for now)
+const targetAnim = document.querySelectorAll('[data-aboutanim]');
+const classAnimation = 'animate';
+
+function aboutMeScroll() {
+    const windowPosition = window.pageYOffset + ((window.innerHeight * 3) / 4)
+    targetAnim.forEach(function(element) {
+            if((windowPosition) > element.offsetTop) {
+                element.classList.add(classAnimation);
+            }
+            else {
+                element.classList.remove(classAnimation);
+            }; 
+    })
+
+}
+
+window.addEventListener('scroll', function() {
+    aboutMeScroll();
+})
